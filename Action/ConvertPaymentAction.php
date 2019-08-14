257538f65a346e744ba0afaedc4d7909d2347194
Albert Mueller <a.mueller@creating-web.de>
2019-08-14 10:25:31 +0200

    set coreshop order number for narrative_text parameter

--- Action/ConvertPaymentAction.php
+++ Action/ConvertPaymentAction.php
@@ -36,14 +36,16 @@ class ConvertPaymentAction implements ActionInterface
 
         $reference = $payment->getNumber();
         $reference = str_replace('ORD-', '', $reference);
-        $reference = str_replace(['-'], ['_'], $reference);
         $reference = substr($reference, 0, 13);
+
+        $narrativeText = explode('_', $payment->getNumber());
+        $narrativeText = reset($narrativeText);
 
         $details['reference'] = $reference;
         $details['order'] = ['orderId' => $reference];
         $details['amount'] = $payment->getTotalAmount();
         $details['currency'] = $payment->getCurrencyCode();
-        $details['narrative_text'] = $payment->getDescription();
+        $details['narrative_text'] = $narrativeText;
 
         $request->setResult((array)$details);
     }
